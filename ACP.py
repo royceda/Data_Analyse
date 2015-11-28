3707de2dae58c00432dd32dccf6ac84526d4614a
royceda <caucricity@gmail.com>
2015-11-28 16:46:37 +0100

    ACP

--- ACP.py
+++ ACP.py
@@ -3,6 +3,8 @@
 import numpy as np
 import matplotlib as mp
 from numpy import linalg as la
+import decimal
+
 
 "Create the scalars which enable to centerize Z "
 def Center(Z):
@@ -17,11 +19,12 @@ def Center(Z):
         X[j] = sum;
     return X;
 
+
 "compute variance"
 def Variance(Z):
     n = len(Z);
     X = Center(Z);
-    S = np.zeros(n);
+    S = np.zeros(n, dtype=np.dtype(decimal.Decimal));
     for j in range (0, n):
         sum = 0.0;
         for i in range(0,n):
@@ -41,33 +44,40 @@ def Centerize(Z):
     return A
 
 
+
+
 def Reduce(Z):
+    n = len(Z);
     A = Z;
+    X = Variance(Z);
+    for i in range(0,n):
+        for j in range (0, n):
+            A[i][j] = A[i][j]/X[j];
+        
     return A;
 
 
 def ACP(Z):
     tmp = Centerize(Z);
-    tmp = Reduce(tmp);
+    #tmp = Reduce(tmp);
 
-    size = np.shape(tmp);
-
+    a = np.shape(tmp)[0];
+    b = np.shape(tmp)[1];
+    
     #SVD decomposition
     U, A, V = np.linalg.svd(tmp);
 
-    S = np.zeros(size[O], size[1])
-    mini = min(size[0], size[1]);
+    S = np.zeros((a, b));
+    mini = min(a, b);
     S[:mini, :mini ]= np.diag(A);
 
     #Factorial coord of peoples (Scores)
     Xi = np.dot(U, S);
 
     #Factorial coord of variables (loadings)
-    Phi = np.dot(V, np.transpose(A));
+    Phi = np.dot(V, np.transpose(S));
 
     return Xi, Phi;
-
-
 
 
 
@@ -85,4 +95,26 @@ print "Centerize(Z): "
 print Centerize(Z);
 
 
-ACP(Z)
+U, S, V = np.linalg.svd(Z, full_matrices=False);
+
+print U;
+print S;
+print V;
+
+print "Xi"
+print ACP(Z)[0]
+
+print "Phi"
+print  ACP(Z)[1]
+
+Xi = ACP(Z)[0];
+Phi = ACP(Z)[1];
+
+
+X =  [row[1] for row in Xi]
+Y =  [row[1] for row in Phi]
+
+import matplotlib.pyplot as plt
+plt.plot(X, Y, 'ro')
+plt.axis([0, 2, 0, 5])
+plt.show()
